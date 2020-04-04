5d9055cd054d5d780d2d159c4b02dc5ca825994f
innovate <quantainnovate@gmail.com>
2020-04-04 19:07:19 +0700

    get date from api and prop function [component vue]

---
+++ components/number_card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card text-white shadow-sm col">
+    <div class="card-body">
+      <div class="card-text">
+        <div class="row">
+          <p class="text-black">{{ topiChospital }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['topiChospital']
+  //   data: () => ({
+  //     object_topicc: null
+  //   }),
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
   <b-container class="bv-example-row">
     <b-row>
-      <b-col id="googleMap" ref="googleMap"></b-col>
+      <!-- <b-col id="googleMap" ref="googleMap"></b-col> -->
+      <b-col md="12">
+        <b-row v-if="object_active_now">
+          <numbercard
+            :topiChospital="object_active_now.Hospitalized"
+          ></numbercard>
+          <numbercard :topiChospital="object_active_now.Confirmed"></numbercard>
+          <numbercard :topiChospital="object_active_now.Deaths"></numbercard>
+          <numbercard :topiChospital="object_active_now.Recovered"></numbercard>
+        </b-row>
+      </b-col>
+      <b-col v-for="(item, index) in items" :key="index" md="12">
+        <p class="col-md">{{ item['properties'].NAME }}</p>
+      </b-col>
+
+      <!-- <b-col-md-12 v-for="item in items" :key="item">
+        <p class="col-md">{{ item['properties'].NAME }}</p>
+      </b-col-md-12>-->
+      <!-- <li v-for="item in items" :key="item">{{ item }}</li> -->
+      <!-- {{ items }} -->
     </b-row>
   </b-container>
 </template>
@@ -10,36 +29,56 @@
 <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjD4swxA8fo0vOcuGItqwlxF2ol2O2cgM"></script> -->
 <script>
 // import Logo from '~/components/Logo.vue'
+import numbercard from '~/components/number_card.vue'
 import axios from 'axios'
 export default {
-  // components: {
-  //   Logo
-  // },
+  components: {
+    //   Logos
+    numbercard
+  },
   data: () => ({
     map: null,
-    items: []
+    items: null,
+    object_active_now: null
   }),
   mounted: function() {
-    this.submitMessage()
-    this.datajs()
-    this.map = new window.google.maps.Map(this.$refs['googleMap'], {
-      center: { lat: -25.344, lng: 131.036 },
-      zoom: 4
-    })
-    new window.google.maps.Marker({
-      position: { lat: -25.344, lng: 131.036 },
-      map: this.map
-    })
+    this.getdata_json()
+    this.getdata_amount_case()
+    // this.map = new window.google.maps.Map(this.$refs['googleMap'], {
+    //   center: { lat: -25.344, lng: 131.036 },
+    //   zoom: 4
+    // })
+    // new window.google.maps.Marker({
+    //   position: { lat: -25.344, lng: 131.036 },
+    //   map: this.map
+    // })
   },
   methods: {
-    submitMessage() {
-      console.log('siomtine') // something
+    getdata_json() {
+      axios
+        .get('/hospital-lab-covid19.json')
+        .then((response, error) => {
+          // console.log(response.data)
+          this.items = response.data
+          console.log(response.data)
+          //todo:: get data complete
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error)
+        })
     },
-    datajs() {
-      axios.get('/hospital-lab-covid19.json').then((response) => {
-        console.log(response.data)
-        //todo:: get data complete
-      })
+    getdata_amount_case() {
+      axios
+        .get('https://covid19.th-stat.com/api/open/today')
+        .then((response, error) => {
+          console.log(typeof response.data)
+          this.object_active_now = response.data
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error)
+        })
     }
   }
 }
