a80464d4bd97a09c33b89855c54974a573a72aa4
innovate <quantainnovate@gmail.com>
2020-04-04 20:02:02 +0700

    get data detail with province

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,69 @@
 <template>
   <b-container class="bv-example-row">
     <b-row>
-      <!-- <b-col id="googleMap" ref="googleMap"></b-col> -->
       <b-col md="12">
         <b-row v-if="object_active_now">
-          <numbercard
-            :topiChospital="object_active_now.Hospitalized"
-          ></numbercard>
-          <numbercard :topiChospital="object_active_now.Confirmed"></numbercard>
-          <numbercard :topiChospital="object_active_now.Deaths"></numbercard>
-          <numbercard :topiChospital="object_active_now.Recovered"></numbercard>
+          <b-col md="3">
+            <div
+              class="d-flex justify-content-center align-items-center p-3 shadow-sm border"
+            >
+              <p class="fs-18 align-self-center m-0">Confirmed:</p>
+              <p class="fs-18 align-self-center m-0">
+                {{ object_active_now.Confirmed }}
+              </p>
+            </div>
+          </b-col>
+          <b-col md="3">
+            <div
+              class="d-flex justify-content-center align-items-center p-3 shadow-sm border"
+            >
+              <p class="fs-18 align-self-center m-0">Hospitalized:</p>
+              <p class="fs-18 align-self-center m-0">
+                {{ object_active_now.Hospitalized }}
+              </p>
+            </div>
+          </b-col>
+          <b-col md="3">
+            <div
+              class="d-flex justify-content-center align-items-center p-3 shadow-sm border"
+            >
+              <p class="fs-18 align-self-center m-0">Recovered:</p>
+              <p class="fs-18 align-self-center m-0">
+                {{ object_active_now.Recovered }}
+              </p>
+            </div>
+          </b-col>
+          <b-col md="3">
+            <div
+              class="d-flex justify-content-center align-items-center p-3 shadow-sm border"
+            >
+              <p class="fs-18 align-self-center m-0">Deaths:</p>
+              <p class="fs-18 align-self-center m-0">
+                {{ object_active_now.Deaths }}
+              </p>
+            </div>
+          </b-col>
         </b-row>
+      </b-col>
+      <b-col md="12" class="text-center my-4">
+        <h5>Amount Province: {{ amt_province }}</h5>
+      </b-col>
+
+      <b-col
+        v-for="(a_patiten, index) in patitent_list.Province"
+        :key="index"
+        md="3"
+      >
+        <div class="card my-2">
+          <div class="card-body">
+            <h4 class="card-title text-center">{{ a_patiten }}</h4>
+            <p class="card-text text-center">{{ index }}</p>
+          </div>
+        </div>
       </b-col>
       <b-col v-for="(item, index) in items" :key="index" md="12">
         <p class="col-md">{{ item['properties'].NAME }}</p>
       </b-col>
-
-      <!-- <b-col-md-12 v-for="item in items" :key="item">
-        <p class="col-md">{{ item['properties'].NAME }}</p>
-      </b-col-md-12>-->
-      <!-- <li v-for="item in items" :key="item">{{ item }}</li> -->
-      <!-- {{ items }} -->
     </b-row>
   </b-container>
 </template>
@@ -39,19 +82,14 @@ export default {
   data: () => ({
     map: null,
     items: null,
-    object_active_now: null
+    object_active_now: null,
+    patitent_list: [],
+    amt_province: null
   }),
   mounted: function() {
     this.getdata_json()
     this.getdata_amount_case()
-    // this.map = new window.google.maps.Map(this.$refs['googleMap'], {
-    //   center: { lat: -25.344, lng: 131.036 },
-    //   zoom: 4
-    // })
-    // new window.google.maps.Marker({
-    //   position: { lat: -25.344, lng: 131.036 },
-    //   map: this.map
-    // })
+    this.getdata_sum_paitaint()
   },
   methods: {
     getdata_json() {
@@ -79,14 +117,21 @@ export default {
           // handle error
           console.log(error)
         })
+    },
+    getdata_sum_paitaint() {
+      axios
+        .get('https://covid19.th-stat.com/api/open/cases/sum')
+        .then((response, error) => {
+          console.log(typeof response.data)
+          this.patitent_list = response.data
+          this.amt_province = Object.values(response.data.Province).length
+          console.log(Object.values(response.data.Province).length)
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error)
+        })
     }
   }
 }
 </script>
-
-<style lang="scss">
-#googleMap {
-  height: 500px;
-  background: gray;
-}
-</style>
